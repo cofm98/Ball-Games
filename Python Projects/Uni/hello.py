cb0b342538abc856761de83294c45a799d9053dd
cofm98 <118582119+cofm98@users.noreply.github.com>
2022-12-04 14:14:50 +1100

    Simple finalisation with credit

--- Python Projects/Uni/hello.py
+++ Python Projects/Uni/hello.py
@@ -1,4 +1,13 @@
+#
+# hello.py
+# Charlie Morrison
+# 4 DEC 22
+#
+# This is a university project for 'Introduction to Porgramming'
+#
+
 import sys
+
 message ="Hello\n"
 message+="world!"
 
@@ -9,10 +18,9 @@ message += '''                 '
                    *       '
               *                *'''
 
-
 message += "\n\nHello, my name is Charlie.\nI have chosen the dataset title 'Power consumption of Tetouan city Data Set,' since I find that electricity is something we utilise every single day no matter who we are, and eventually leading into a system which will hopefully analyse this data will certainly have some interesting results."
-message += "\nI have programmed previously in my life, I haven't for quite some time but it is good to see that the base rules still haven't changed.\n"
-message += "\nThe ASCII art surrounding this wall of text is space themed. I find the \n"
+message += "\nI have programmed previously in my life, I haven't for quite some time but it is good to see that the base rules still haven't changed."
+message += "\nThe ASCII art surrounding this wall of text is space themed. I find the vastness of space pure and fascinating. \n\n"
 
 message += '''   *   '*
            *
@@ -26,8 +34,8 @@ message += '''   *   '*
          :                  - --+- -
          !           .          !
          |        .             .
-         |_         +
-      ,  | `.
+        _|_         +
+      ,' | `.
 --- --+-<#>-+- ---  --  -
       `._|_,'
          T
@@ -35,4 +43,5 @@ message += '''   *   '*
          !
          :         . : 
          .       *'''
+
 sys.stdout.write(message)
