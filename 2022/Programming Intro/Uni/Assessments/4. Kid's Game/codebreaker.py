df4b2e4c939ebc80861fcd0876057d5463b46249
cofm98 <118582119+cofm98@users.noreply.github.com>
2022-12-22 15:12:49 +1000

    Updates for 22/12/22

--- 2022/Programming Intro/Uni/Assessments/4. Kid's Game/codebreaker.py
+++ 2022/Programming Intro/Uni/Assessments/4. Kid's Game/codebreaker.py
@@ -1,32 +1,50 @@
 import sys, random
 
 possibleCodes = ["A","B","C","D","E","F"]
+currentHint = []
 guesses = []
 code = []
 
 for number in range(5):
     code.append(possibleCodes[random.randrange(0,5)])
 
-def compare() -> bool:
-    if("".join(code) == userInput):
-        print("Code was correct!")
-        return True
+def compare(guess : str) -> bool:
+    if(len(guess) > 5):
+        global currentHint
+        guess = guess.upper()[0:5]
+        for eachLetter in guess:
+            if(eachLetter not in possibleCodes):
+                sys.stdout.write("Letter [{}] is not in dictionary [A-F]".format(eachLetter))
+                return False
+        guesses.append([*guess])
+        if("".join(code) == guess):
+            print("Code was correct!")
+            return True
+        else:
+            outCode = []
+            for eachAnswerID in range(len(code)):
+                if(code[eachAnswerID] == guess[eachAnswerID]):
+                    outCode.append(code[eachAnswerID])
+                else:
+                    outCode.append("x")
+            currentHint = outCode
+            return False
     else:
-        outCode = []
-        for eachAnswerID in range(len(code)):
-            #print("Checking {} against {} at {}".format(userInput[eachAnswerID], code[eachAnswerID], str(eachAnswerID)))
-            if(code[eachAnswerID] == userInput[eachAnswerID]):
-                outCode.append(code[eachAnswerID])
-            else:
-                outCode.append("x")
-                
-        print("-".join(outCode))
+        sys.stdout.write("Less than 5 letters!\n")
         return False
 
 correct = False
 while not correct:
-    
-    userInput = sys.stdin.readline()[0:5]
-    guesses.append(userInput)
-    correct = compare()
-    sys.stdin.flush()
+    if(len(guesses) < 8):
+        sys.stdout.write("\nThe code is 5 letters [X-X-X-X-X]. Any letter from A to F.\nBe warned! You only have 8 guesses!\n")
+        sys.stdout.write("      " + "-".join(currentHint) + "\n")
+        if(len(guesses) > 0):
+            for eachGuessID in range(len(guesses)):
+                sys.stdout.write("[{}] - ".format(str(eachGuessID+1)) + "-".join(guesses[eachGuessID]) + "\n")
+        userInput = sys.stdin.readline()
+        correct = compare(userInput)
+        sys.stdin.flush()
+        sys.stdout.flush()
+    else:
+        sys.stdout.write("\nMore than 8 guesses have been attempted!")
+        correct = True
